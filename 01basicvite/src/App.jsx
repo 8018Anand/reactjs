d4aced6347e3a86c8f23e1a48b58881e24d20a89
Anand Kumar Padakala <anandkumarpadakala786@outlook.com>
2025-01-01 04:16:46 +0530

    created custom react file and explored how react file are use js to create elements on the ui and some how try to replicate the same things on the vite-reactjs app

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -1,13 +1,18 @@
+import React from "react";
 import Youtube from "./Youtube";
 
+
 function App() {
+  const name = "React";
   return (
     <>
-      <h1>Vite react app</h1>
-      <h1>Vite react app</h1>
+      <h1>Vite react app {2+2}</h1>
+      <h1>Vite react app {name}</h1>
       <Youtube />
     </>
   );
 }
+
+
 
 export default App;
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,9 +1,40 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
 
-    <App />
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://www.google.com",
+    target: "_blank",
+  },
+  children: "Click me to visit google",
+};
 
+function MyApp() {
+    return (
+      <div>
+        <h1>custom react app</h1>
+      </div>
+    );
+}
+
+const AnotherElement = (
+  <a href="https://www.google.com" target="_blank">
+    Visit google
+  </a>
+);
+
+
+const areactElement = React.createElement(
+    'a',
+    {href: 'https://www.google.com', target: '_blank'},
+    'Click me to visit google'
 )
+
+createRoot(document.getElementById("root")).render(
+  // <MyApp />
+  //   AnotherElement
+  <App />
+);
